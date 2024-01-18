445de26ff2c62c0be61e964cbab210fe5b32fea9
oasui <133706215+oasui@users.noreply.github.com>
2024-01-19 05:44:23 +0800

    Update str.py

--- PyroUbot/core/plugins/str.py
+++ PyroUbot/core/plugins/str.py
@@ -22,12 +22,12 @@ async def ping_cmd(client, message):
     if client.me.is_premium:
         _ping = f"""
 <b><emoji id={emot_pong}>🏓</emoji>ᴘᴏɴɢ:</b> <code>{str(delta_ping).replace('.', ',')} ms</code>
-<b><emoji id={emot_mention}>👑</emoji>ᴍᴇɴᴛɪᴏɴ:</b> <code>ᴠ ɪ ʌ ɴ  ᴜʙᴏᴛ</code>
+<b><emoji id={emot_mention}>👑</emoji>ᴍᴇɴᴛɪᴏɴ:</b> <code>ᴠɪʌɴ  ᴜʙᴏᴛ</code>
 """
     else:
         _ping = f"""
 <b>ᴘᴏɴɢ:</b> <code>{str(delta_ping).replace('.', ',')} ms</code>
-<b>ᴍᴇɴᴛɪᴏɴ:</b> <code>v1abingubot</code>
+<b>ᴍᴇɴᴛɪᴏɴ:</b> <code>ᴠɪʌɴ  ᴜʙᴏᴛ</code>
 """
     await message.reply(_ping)
 
