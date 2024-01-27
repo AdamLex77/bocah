fc12f28f271c878e881713760114f019ed124d16
oasui <133706215+oasui@users.noreply.github.com>
2024-01-28 05:30:50 +0800

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -14,7 +14,7 @@ OWNER_ID = int(os.getenv("OWNER_ID", "2025721293"))
 
 LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002048436505"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002048436505,-1001473548283").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001473548283").split()))
 
 MAX_BOT = int(os.getenv("MAX_BOT", "550"))
 
