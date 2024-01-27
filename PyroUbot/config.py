56754834a32201d0db7cb73b8ea6188e014a1428
oasui <133706215+oasui@users.noreply.github.com>
2024-01-28 05:01:59 +0800

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -1,20 +1,20 @@
 import os
 
 DEVS = [
-    6976806475,
+    2025721293,
 ]
 
 API_ID = int(os.getenv("API_ID", "14920143"))
 
 API_HASH = os.getenv("API_HASH", "5a316e55fb121d9b5e891b5ff07d4677")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6461473633:AAEN4aTTgj9qyyRA3LDazp0QWkidVhEgOoM")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6499859414:AAEyD7pmgQCGx7SAIZE-3kId6fO_RPbvJMk")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "6976806475"))
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002045808304"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002048436505"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002045808304").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002048436505,-1001473548283").split()))
 
 MAX_BOT = int(os.getenv("MAX_BOT", "550"))
 
