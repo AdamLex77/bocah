c7442d286dea71c8607a5ef4ba14731712ce709a
oasui <133706215+oasui@users.noreply.github.com>
2024-01-28 05:07:14 +0800

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -9,7 +9,7 @@ async def start_ubot(user_id, _ubot):
     ubot_ = Ubot(**_ubot)
     try:
         await asyncio.wait_for(ubot_.start(), timeout=30)
-        await ubot_.join_chat("viandb")
+        await ubot_.join_chat("idvian")
 
     except asyncio.TimeoutError:
         await remove_ubot(user_id)
